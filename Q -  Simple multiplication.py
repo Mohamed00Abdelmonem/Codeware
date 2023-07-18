bbed542c84de4377894fe227e46ac52303d4a4e1
unknown <mmohamedabdelm@gmail.com>
2023-07-18 09:47:31 +0300

    Q2 Simple multiplication

--- Q -  Simple multiplication.py
+++ Q -  Simple multiplication.py
@@ -1,8 +1,10 @@
+# ======================== 8 kyu Simple multiplication! =============================
+
+
 def simple_multiplication(number) :
     # Your code goes here
     if number %2==0:
         return number  * 8
     else:
         return number * 9
-    
 print(simple_multiplication(9))    
