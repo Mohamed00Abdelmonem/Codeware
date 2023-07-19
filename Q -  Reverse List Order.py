62d70251511dee1b56b37eea9a26e629225069bd
unknown <mmohamedabdelm@gmail.com>
2023-07-19 08:28:09 +0300

    Q3

---
+++ Q -  Reverse List Order.py
@@ -0,0 +1,11 @@
+# ======================== 8 kyu Reverse List Order=============================
+
+def reverse_list(l):
+    'return a list with the reverse order of l' 
+    return l[::-1]
+print(reverse_list([3, 4, 2,4,5,6,9]))
+
+
+# Examples (Input -> Output)
+# * [1, 2, 3, 4]  -> [4, 3, 2, 1]
+# * [9, 2, 0, 7]  -> [7, 0, 2, 9]    
---
+++ Q -  Volume of a Cuboid.py
@@ -0,0 +1,8 @@
+# ======================== 8 kyu Volume of a Cuboid =============================
+
+
+def get_volume_of_cuboid(length, width, height):
+    # Code goes here...
+   
+    return  length * width * height
+print(get_volume_of_cuboid())
